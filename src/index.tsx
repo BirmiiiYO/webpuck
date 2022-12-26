47c5c3130dc23fa2819b0707ea7f9ec3c0f3e8e0
BirmiiiYo <avramenkoa773@gmail.com>
2022-12-26 14:48:27 +0300

    add husky

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+// eslint-disable-next-line prettier/prettier
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(<App />)
